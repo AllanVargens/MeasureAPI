8452cdeb0043cb3ce5e0a25be8177a96095b8af8
AllanVargens <allanvargens@gmail.com>
2024-08-30 20:24:51 -0300

    feat: Files Deleted from stay just the docker-compose

--- src/gemini/application/gemini.constant.ts
+++
@@ -1,3 +0,0 @@
-export const GEMINI_PRO_MODEL = 'GEMINI_PRO_MODEL';
-export const GEMINI_PRO_VISION_MODEL = 'GEMINI_PRO_VISION_MODEL';
-export const GEMINI_PRO_VISION_1_5_FLASK_MODEL= "gemini-1.5-flash";
